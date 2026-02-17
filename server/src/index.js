d7adcbb194600ea016f879198c384debe7356adb
Ralf Siewert <ralf@actyvy.st>
2026-02-17 16:27:56 +0100

    add react client

--- server/src/index.js
+++ server/src/index.js
@@ -1,44 +1,46 @@
-const express = require('express');
-const db = require('./db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { generateAccessToken, generateRefreshToken } = require('./jwt');
-const authenticateToken = require('./auth');
+const express = require("express");
+const db = require("./db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { generateAccessToken, generateRefreshToken } = require("./jwt");
+const authenticateToken = require("./auth");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from myApp!');
+app.get("/", (req, res) => {
+  res.send("Hello from myApp!");
 });
 
-app.post('/register', async (req, res) => {
-  console.log('/register called');
+app.post("/register", async (req, res) => {
+  console.log("/register called");
 
   const { email, password } = req.body;
-  console.log('🚀 ~  email, password :', email, password);
+  console.log("🚀 ~  email, password :", email, password);
 
   if (!email || !password) {
-    console.log('email/password do not exist');
-    return res.status(400).json({ error: 'Email and password required!' });
+    console.log("email/password do not exist");
+    return res.status(400).json({ error: "Email and password required!" });
   }
   if (password.length < 6) {
-    console.log('password too short');
+    console.log("password too short");
     return res
       .status(400)
-      .json({ error: 'Password must be at least 6 characters long!' });
+      .json({ error: "Password must be at least 6 characters long!" });
   }
   if (!/\S+@\S+\.\S+/.test(email)) {
-    console.log('invalid email');
-    return res.status(400).json({ error: 'Invalid email format!' });
+    console.log("invalid email");
+    return res.status(400).json({ error: "Invalid email format!" });
   }
 
   try {
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log('password hashed');
+    console.log("password hashed");
 
     const query = `
       INSERT INTO users (email, password)
@@ -48,70 +50,76 @@ app.post('/register', async (req, res) => {
 
     db.query(query, [email, hashedPassword], (err) => {
       if (err) {
-        if (err.code === '23505') {
-          console.log('User exists');
-          return res.status(400).json({ error: 'User already exists' });
+        if (err.code === "23505") {
+          console.log("User exists");
+          return res.status(400).json({ error: "User already exists" });
         }
-        console.log('Database error:', err);
-        return res.status(500).json({ error: 'Database error' });
+        console.log("Database error:", err);
+        return res.status(500).json({ error: "Database error" });
       }
-      console.log('success');
-      res.status(201).json({ message: 'User registered successfully' });
+      console.log("success");
+      res.status(201).json({ message: "User registered successfully" });
     });
   } catch (err) {
-    console.log('server error');
+    console.log("server error");
     res.status(500).json({ error: err.message });
   }
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
+  console.log(req.body);
   const { email, password } = req.body;
-  console.log('🚀 ~  email, password:', email, password);
+  console.log("🚀 ~  email, password:", email, password);
 
   const query = `SELECT * FROM users WHERE email = $1`;
 
   db.query(query, [email], async (err, result) => {
     if (err) {
-      return res.status(500).json({ error: 'Server error' });
+      console.log("server error");
+      return res.status(500).json({ error: "Server error" });
     }
 
     const user = result.rows[0];
-    console.log('🚀 ~ user:', user);
+    console.log("🚀 ~ user:", user);
     if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: "Invalid credentials" });
     }
 
     // Compare password
     const isValid = await bcrypt.compare(password, user.password);
 
     if (!isValid) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      console.log("Invalid credentials");
+      return res.status(401).json({ error: "Invalid credentials" });
     }
     // Generate JWT tokens
     const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
+
+    // We will do refreshTokens later:
+    // -----------------------------
+    // const refreshToken = generateRefreshToken(user);
 
     // 🔥 Hash refresh token before storing
-    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    // const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
 
     // Save hashed refresh token to DB
-    try {
-      await db.query(
-        `INSERT INTO refresh_tokens (token, user_id) VALUES ($1, $2)`,
-        [hashedRefreshToken, user.id],
-      );
-    } catch (dbErr) {
-      console.log('Database error saving refresh token:', dbErr);
-      return res.status(500).json({ error: 'Server error' });
-    }
+    // try {
+    //   await db.query(
+    //     `INSERT INTO refresh_tokens (token, user_id) VALUES ($1, $2)`,
+    //     [hashedRefreshToken, user.id],
+    //   );
+    // } catch (dbErr) {
+    //   console.log("Database error saving refresh token:", dbErr);
+    //   return res.status(500).json({ error: "Server error" });
+    // }
+    // ------------------------------
 
     // Send original token to client
     res.json({
-      message: 'Login successful',
+      message: "Login successful",
       userId: user.id,
       email: user.email,
       accessToken,
-      refreshToken,
     });
   });
 });
@@ -164,7 +172,7 @@ app.post('/login', (req, res) => {
 //   }
 // });
 
-app.get('/users', authenticateToken, (req, res) => {
+app.get("/users", authenticateToken, (req, res) => {
   db.query(`SELECT email FROM users`, (err, result) => {
     if (err) {
       return res.status(500).json({ error: err.message });
